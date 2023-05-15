2c7551d0a456d14cfa8b320a9e41461be1faa114
Robert HAK <robhak@e1r8p10.clusters.42paris.fr>
2023-05-15 15:54:59 +0200

    trop long

--- ft_printf2.c
+++ ft_printf2.c
@@ -6,11 +6,13 @@
 /*   By: robhak <robhak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:34:42 by robhak            #+#    #+#             */
-/*   Updated: 2023/05/15 15:20:29 by robhak           ###   ########.fr       */
+/*   Updated: 2023/05/15 15:50:04 by robhak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf_function.h"
+
+//printf de null renvoie -1
 
 int	print_format(const char *format, va_list args)
 {
@@ -22,11 +24,32 @@ int	print_format(const char *format, va_list args)
 		if (format[i] == '%')
 		{
 			i++;
+			if (format[i] == 'c')
+				cpt += print_char(va_arg(args, int));
+			else if (format[i] == 's')
+				cpt += print_str(va_arg(args, char *));
+			else if (format[i] == 'p')
+				cpt += print_pointer(va_arg(args, void *));
+			else if (format[i] == 'd' || format[i] == 'i')
+				cpt += print_decimal(va_arg(args, int));
+			else if (format[i] == 'u')
+				cpt += print_unsigned_decimal(va_arg(args, unsigned int));
+			else if (format[i] == 'x')
+				cpt += print_hexadecimal(va_arg(args, unsigned int), "01233456789abcdef");
+			else if (format[i] == 'X')
+				cpt += print_hexadecimal(va_arg(args, unsigned int), "01233456789ABCDEF");
+			else if (format[i] == '%')
+				cpt += print_char('%');
 		}
+		else
+			cpt += print_char(format[i]);
+		i++;
 	}
+	return (cpt);
 }
 
 int	ft_printf(const char *format, ...);
 {
-	
+	if (!format)
+		return (-1);
 }
--- printf_function.h
+++ printf_function.h
@@ -6,7 +6,7 @@
 /*   By: robhak <robhak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 16:26:39 by robhak            #+#    #+#             */
-/*   Updated: 2023/05/15 13:47:08 by robhak           ###   ########.fr       */
+/*   Updated: 2023/05/15 15:32:56 by robhak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,11 @@ int	ft_putchar(char c);
 int	ft_putstr(char *str);
 int	ft_putnbr(int n);
 int ft_putnbr_base(unsigned int n, char *base);
+int	print_char(char c);
+int	print_str(char *str);
+int	print_pointer(void *ptr);
+int	print_decimal(int n);
+int	print_unsigned_decimal(unsigned int n);
+int	print_hexadecimal(unsigned int n, const char *base);
 
 #endif
